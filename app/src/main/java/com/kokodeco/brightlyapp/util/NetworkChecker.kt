56a50341b9058aec964486cdcd136da2febdb472
itsmekoko <cocotamarian@yahoo.com>
2023-12-12 13:05:46 +0200

    DataState file cleaning

--- app/src/main/java/com/kokodeco/brightlyapp/util/NetworkChecker.kt
+++ app/src/main/java/com/kokodeco/brightlyapp/util/NetworkChecker.kt
@@ -2,12 +2,15 @@ package com.kokodeco.brightlyapp.util
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.core.content.ContextCompat
 import javax.inject.Inject
 
 class NetworkConnectivityChecker @Inject constructor(private val context: Context) {
     fun hasNetworkAccess(): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val activeNetwork = connectivityManager.activeNetworkInfo
-        return activeNetwork?.isConnectedOrConnecting == true
+        val connectivityManager = ContextCompat.getSystemService(context, ConnectivityManager::class.java)
+        val network = connectivityManager?.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
     }
 }
